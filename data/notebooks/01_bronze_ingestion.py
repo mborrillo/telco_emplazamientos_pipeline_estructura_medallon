6a1677f9ebb147068c90ca22521649946baf7efd
Marcos  Borrillo <68borrismar@gmail.com>
2026-01-12 21:43:16 +0100

    Create 01_bronze_ingestion.py

---
+++ data/notebooks/01_bronze_ingestion.py
@@ -0,0 +1,8 @@
+- 01_bronze_ingestion.py`  
+[01_bronze_ingestion.py](https://github.com/user-attachments/files/24572935/01_bronze_ingestion.py)
+
+  - Lee automáticamente todos los ficheros Excel de un volumen de Databricks.  
+  - Fuerza los campos a tipo texto para evitar problemas de tipado.  
+  - Evita la conversión automática a `NaN` y crea tablas `raw_emplazamientos_centros_tx`, `raw_emplazamientos_infra` y `raw_emplazamientos_sitios_ran`.
+
+  
